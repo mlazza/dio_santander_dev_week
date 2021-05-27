b762813bd083c586287db7e51fa1abdc79095a32
mlazza <mlazzarotti@gmail.com>
2021-05-27 13:11:14 -0300

    first commit

---
+++ app/src/main/java/com/lazzarotti/santander_dev_week/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lazzarotti.santander_dev_week
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
